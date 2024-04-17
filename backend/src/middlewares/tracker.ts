b0978a1295a35e13282879411357c8fb8b1ae268
Mahmoud Nasr <m9nasr@outlook.com>
2024-04-17 17:42:10 +0200

    Update tracker.ts

--- backend/src/middlewares/tracker.ts
+++ backend/src/middlewares/tracker.ts
@@ -1,14 +1,22 @@
-import { NextFunction, Request, Response } from 'express';
+import {NextFunction, Request, Response} from 'express';
 
 import SimpleCrypto from 'simple-crypto-js';
 import catchAsync from './errors/catchAsync';
-import { config } from '@/config';
+import {config} from '@/config';
 
 const Decryptor = catchAsync(async (req: Request, _res: Response, next: NextFunction) => {
 	const {data} = req.body;
 
 	const Crypto = new SimpleCrypto(config.extension.secret);
 	const Decrypted = Crypto.decrypt(data);
+	if (!Decrypted) return next('Invalid Data');
+
+	// Check if req.body is has error property, if it does, then it's an error object
+	if (req.body.hasOwnProperty('error')) {
+		req.body.data = Decrypted;
+		return next();
+	}
+
 	req.body = Decrypted;
 	next();
 });
