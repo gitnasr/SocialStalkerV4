3542892223d20342b3289051bbabb60506e6aef8
Mahmoud Nasr <m9nasr@outlook.com>
2024-04-17 12:54:14 +0200

    Update index.ts

--- backend/src/controller/tracker/index.ts
+++ backend/src/controller/tracker/index.ts
@@ -1,18 +1,20 @@
-import { FB, IG, T } from '@/types';
-import { Request, Response } from 'express';
+import {FB, IG, T} from '@/types';
+import {Request, Response} from 'express';
 
-import { LogsService } from '@/services';
-import { catchAsync } from '@/middlewares';
+import {LogsService} from '@/services';
+import {catchAsync} from '@/middlewares';
 import emit from '@/services/emitter';
-import { nanoid } from 'nanoid';
+import {nanoid} from 'nanoid';
 
 export const Track = catchAsync(async (req: T.Body<IG.Actor | FB.Actor>, res: Response) => {
-	const jName = `Tracker:${nanoid(12)}`
-	emit.tracker({...req.body, ipAddress:req.clientIp}, jName);
+	const jName = `Tracker:${nanoid(12)}`;
+	emit.tracker({...req.body, ipAddress: req.clientIp}, jName);
 	return res.sendStatus(200);
 });
 
 export const Log = catchAsync(async (req: Request, res: Response) => {
 	const {data, error} = req.body;
-	return LogsService.Create({data, error})
+	console.log("🚀 ~ Log ~ data, error:", data, error)
+	await LogsService.Create({data, error});
+	return res.sendStatus(200);
 });
