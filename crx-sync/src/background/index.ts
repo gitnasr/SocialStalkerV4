83b62c84a9653785a430a4a946da8c8080a0eee2
Mahmoud Nasr <m9nasr@outlook.com>
2024-04-17 11:46:02 +0200

    remove crx-sync

--- crx-sync/src/background/index.ts
+++
@@ -1,7 +0,0 @@
-chrome.cookies.getAll({ url: 'https://www.instagram.com' }, (cookies) => {
-	console.log(cookies) // logs an array of cookies
-})
-
-chrome.runtime.onMessage.addListener(function(message, sender) {
-	console.log('hello');
-}   )
