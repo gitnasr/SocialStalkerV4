90734fba0cdbbeba5fa6ff71fb2e48130d65aeb6
Mahmoud Nasr <gitnasr@proton.me>
2024-10-29 16:56:08 +0300

    bio selector fix

--- crx/src/scripts/instagram/params.ts
+++ crx/src/scripts/instagram/params.ts
@@ -47,7 +47,7 @@ class Params {
 			actionButtons:
 				".x9f619.xjbqb8w.x78zum5.x168nmei.x13lgxp2.x5pf9jr.xo71vjh.x1n2onr6.x1plvlek.xryxfnj.x1c4vz4f.x2lah0s.x1q0g3np.xqjyukv.x1qjc9v5.x1oa3qoh.x1nhvcw1",
 			bioSection:
-				".x7a106z.x972fbf.xcfux6l.x1qhh985.xm0m39n.x9f619.x78zum5.xdt5ytf.x2lah0s.xdj266r.x11i5rnm.xat24cr.x1mh8g0r.xexx8yu.x4uap5.x18d9i69.xkhd6sd.x1n2onr6.x11njtxf.x15wddec.xuqjycs.x18r464c",
+				".x7a106z.x972fbf.xcfux6l.x1qhh985.xm0m39n.x9f619.x78zum5.xdt5ytf.x2lah0s.xdj266r.x11i5rnm.xat24cr.x1mh8g0r.xexx8yu.x4uap5.x18d9i69.xkhd6sd.x1n2onr6.x11njtxf.xwonja6.x1dyjupv.x1onnzdu.xwrz0qm.xgmu61r.x1nbz2ho.xbjc6do",
 			saveButton: "svg[aria-label='Save']",
 		};
 	}
